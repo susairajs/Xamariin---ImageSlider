fd0885db24f341636e52bccbaa4637084b7c1ea4
DelpinSusaiRaj <susairajs@outlook.com>
2018-08-22 23:34:07 +0530

    image silder complete

---
+++ XamarinImageSlider/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamd.ImageCarousel.Forms.Plugin.Abstractions;
+namespace XamarinImageSlider
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<FileImageSource> imageSources = new ObservableCollection<FileImageSource>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            imageSources.Add("XamarinmonkeyLogo.png");
+            imageSources.Add("github.png");
+            imageSources.Add("microsoft.png");
+
+
+            imgSlider.Images = imageSources;
+        }
+    }
+}
